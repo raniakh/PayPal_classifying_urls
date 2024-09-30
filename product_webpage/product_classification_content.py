7f1e72795f2a5d88191eeaa8e00154d2f1b17a4a
Rania Khoury <rania.kh3107@gmail.com>
2024-09-30 20:01:34 +0300

    classification based on content

--- product_webpage/product_classification_content.py
+++ product_webpage/product_classification_content.py
@@ -12,78 +12,106 @@ from urllib.parse import urlparse
 import time
 from cachetools import cached, TTLCache
 from joblib import Parallel, delayed
+from multiprocessing import Manager
 
 # cache with 1000 max items and 10 minutes time-to-live
 cache = TTLCache(maxsize=1000, ttl=600)
 
 pattern = re.compile(
-    r'/cart/*$|/checkout/*$|'
-    r'/login/*$|/signup/*$|/log-in/*$|/account/*$|/my-account/*$|'
-    r'/return-policy/*$|/privacy-policy/*$|/terms-of-service/*$|/refund-policy/*$|/shipping-policy/*$|/terms-conditions'
-    r'/*$|/terms-and-conditions/*$|'
-    r'/reviews/*$|/all-reviews/*$|'
-    r'/contact-us/*$|/contact/*$|/contactus/*$|/customer-service/*$|'
-    r'/aboutus/*$|/about-us/*$|/about/*$|'
-    r'/index.php/*$|/home/*$|'
-    r'/blog/*$|/news/*$|/our-brands/*$|'
-    r'/search/*$|/collections/*$|/merch/*$|'
-    r'/collections/|/category/|/category/collections/'
+    r'/cart/*|/checkout/*$|'
+    r'/login/*$|/signup/*|/log-in/*|/account/*|/my-account/*$|/register/*'
+    r'/return-policy/*|/privacy-policy/*|/terms-of-service/*|/refund-policy/*|/shipping-policy/*|/terms-conditions'
+    r'/*|/terms-and-conditions/*|'
+    r'/reviews/*|/all-reviews/*|'
+    r'/contact-us/*|/contact/*$|/contactus/*|/customer-service/*|'
+    r'/aboutus/*$|/about-us/*|/about/*|'
+    r'/index.php/*$|/home/*|'
+    r'/blog/*|/blogs/*|/news/*|/our-brands/*$|'
+    r'/search/*$|/merch/*$|/donate/*|/fag/*|/forums/*|'
+    r'/collections/*|/category/*|/product-category/*|/product-brands/*'
 )
+# TODO at least two keywords present in the html
+# TODO check if can see if attribute visible on page
+keywords = ['buy now', 'more payment options', 'buy with apple pay', 'pay now',
+            'pay with paypal', 'buy with',
+            'product description', 'product specifications', 'product information',
+            'customer reviews', 'product reviews',
+            'you may also like', 'related products', 'more from this collection', 'other products',
+            'request a quote', 'view store information']
+# 'ratings', 'share product', 'reviews', 'sku', 'add to cart'
+# 'credit card', 'debit card', 'debit or credit card', 'sold out', 'out of stock', 'in stock',
 
-keywords = ['buy now', 'add to cart', 'more payment options', 'buy with apple pay',
-            'pay with paypal', 'buy with', 'sold out', 'out of stock', 'in stock',
-            'credit card', 'debit card', 'debit or credit card',
-            'description', 'specs', 'product description', 'product specifications',
-            'product information',
-            'reviews', 'customer reviews', 'ratings', 'share product',
-            'you may also like', 'related product', 'more from this collection',
-            'sku', 'request a quote']
-
-
-def matchRegex(chunk):
-    chunk['regex_match'] = chunk['sublinks'].str.contains(pattern)
-    chunk.loc[chunk['regex_match'], 'Product Page'] = -1
-    return chunk.drop(columns=['regext_match'])
-
-
-def detectHomepage(chunk):
-    def isHomepage(url):
-        parsed_url = urlparse(url)
-        return parsed_url.path in ['', '/', '/home', '/index.php']
-
-    chunk['homepage_match'] = chunk['sublinks'].apply(isHomepage)
-    chunk.loc[chunk['homepage_match'], 'Product Page'] = -1
-    return chunk.drop(column=['homepage_match'])
+headers = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) '
+                  'Chrome/91.0.4472.124 Safari/537.36'
+}
 
 
-@cached(cache)
-async def fetchContent(url):
-    try:
-        async with aiohttp.ClientSession() as session:
-            async with session.get(url, timeout=5) as response:
+class URLProcessor:
+    def __init__(self, timeout_counter):
+        self.timeout_counter = timeout_counter
+
+    def matchRegex(self, chunk):
+        chunk['regex_match'] = chunk['sublinks'].str.contains(pattern)
+        chunk.loc[chunk['regex_match'], 'Product Page'] = -1
+        return chunk.drop(columns=['regex_match'])
+
+    def detectHomepage(self, chunk):
+        def isHomepage(url):
+            parsed_url = urlparse(url)
+            return parsed_url.path in ['', '/', '/home', '/index.php']
+
+        chunk['homepage_match'] = chunk['sublinks'].apply(isHomepage)
+        chunk.loc[chunk['homepage_match'], 'Product Page'] = -1
+        return chunk.drop(columns=['homepage_match'])
+
+    @cached(cache)
+    async def fetchContent(self, url, session):
+        try:
+            async with session.get(url, headers=headers, timeout=120) as response:
                 if response.status == 200:
                     return await response.text()
-                return ''
-    except Exception:
-        return ''
+                else:
+                    print(f"Failed to fetch {url}, statuse Code: {response.status}")
+                    return ''
+        except asyncio.TimeoutError:
+            print(f"Timeout fetching {url}")
+            self.timeout_counter.value += 1
+            return ''
+        except Exception as e:
+            print(f"Error fetching {url}: {str(e)}")
+            return ''
 
+    # Extract webpage content and classify based on bag of words
+    async def extractAndClassify_async(self, chunk):
+        async with aiohttp.ClientSession() as session:
+            tasks = [self.fetchContent(row['sublinks'], session) for index, row in chunk.iterrows()]
+            contents = await asyncio.gather(*tasks)
 
-# Extract webpage content and classify based on bag of words
-async def extractAndClassify_async(chunk):
-    tasks = [fetchContent(row['sublinks']) for index, row in chunk.iterrows()]
-    contents = await asyncio.gather(*tasks)
+            for i, content in enumerate(contents):
+                if any(key_word in content.lower() for key_word in keywords):
+                    chunk.iloc[i, chunk.columns.get_loc('Product Page')] = 1
 
-    for i, content in enumerate(contents):
-        if any(key_word in content.lower() for key_word in keywords):
-            chunk.iloc[i, chunk.columns.get_loc('Product Page')] = 1
+        return chunk
 
-    return chunk
+    # Wrapper to run async function in sync code
+    def extractAndClassifyWrapper(self, chunk):
+        loop = asyncio.get_event_loop()
+        return loop.run_until_complete(self.extractAndClassify_async(chunk))
 
+    def processChunk(self, chunk):
+        chunk = self.matchRegex(chunk)
+        chunk = self.detectHomepage(chunk)
 
-# Wrapper to run async function in sync code
-def extractAndClassifyWrapper(chunk):
-    loop = asyncio.get_event_loop()
-    return loop.run_until_complete(extractAndClassify_async(chunk))
+        filtered_chunk = chunk[chunk['Product Page'] == 0]
+        if not filtered_chunk.empty:
+            filtered_chunk = self.extractAndClassifyWrapper(filtered_chunk)
+
+        chunk.update(filtered_chunk)
+        return chunk
+
+    def getTimeOutCount(self):
+        return self.timeout_counter.value
 
 
 def chunkDataframe(df, chunk_size):
@@ -91,30 +119,26 @@ def chunkDataframe(df, chunk_size):
         yield df.iloc[start: start + chunk_size]
 
 
-def processChunk(chunk):
-    chunk = matchRegex(chunk)
-    chunk = detectHomepage(chunk)
-
-    filtered_chunk = chunk[chunk['Product Page'] == 0]
-    if not filtered_chunk.empty:
-        filtered_chunk = extractAndClassifyWrapper(filtered_chunk)
-
-    chunk.update(filtered_chunk)
-    return chunk
-
-
-def processDataFrameInChunks(df, chunk_size=1000, n_jobs=-1):
+def processDataFrameInChunks(df, chunk_size=1000, n_jobs=-1, processor=None):
     chunks = list(chunkDataframe(df, chunk_size))
     # Process chunks in parallel
-    results = Parallel(n_jobs=n_jobs)(delayed(processChunk)(chunk) for chunk in chunks)
+    results = Parallel(n_jobs=n_jobs)(delayed(processor.processChunk)(chunk) for chunk in chunks)
 
     return pd.concat(results)
 
 
 if __name__ == '__main__':
     start_time = time.time()
+
     data = pd.read_csv('../output/productpage_classification_based_regex.csv')
     data_filtered = data[data['Product Page'] == 0].copy()
-    data_processed = processDataFrameInChunks(data_filtered)
-    print("--- %.2f seconds ---" % (time.time() - start_time))
-    data_processed.to_csv('./output/productpage_classification_content_based.csv', index=False)
+
+    with Manager() as manager:
+        timeout_counter = manager.Value('i', 0)
+        processor = URLProcessor(timeout_counter)
+
+        data_processed = processDataFrameInChunks(df=data_filtered, processor=processor)
+        print(f"time out count: {processor.getTimeOutCount()}")
+        print("--- %.2f seconds ---" % (time.time() - start_time))
+
+        data_processed.to_csv('../output/productpage_classification_content_based.csv', index=False)
